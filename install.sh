2d930e9be93ba198dd23d472f27cf2aa152a0dbe
bboti86 <bboti86@gmail.com>
2018-06-29 21:53:22 +0200

    adding install script

---
+++ install.sh
@@ -0,0 +1,21 @@
+echo Adding atom repository
+sudo add-apt-repository ppa:webupd8team/atom
+sudo apt update
+
+echo Installing Atom
+sudo apt install atom
+
+echo Installing necessary packages for Home Assistant
+sudo apt-get install python3-pip python3-dev python3-venv
+sudo apt-get install libssl-dev libxml2-dev libxslt1-dev libjpeg-dev libffi-dev libudev-dev zlib1g-dev
+
+echo Installing Atom packages
+python3 -m pip install 'python-language-server[all]'
+apm install atom-ide-ui ide-python minimap atom-file-icons atom-material-syntax atom-material-ui
+
+echo Cloning repository
+mkdir ~/hass
+git clone https://github.com/bboti86/home-assistant.git ~/hass
+cd ~/hass
+git remote add upstream https://github.com/home-assistant/home-assistant.git
+
